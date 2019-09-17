13432a9f19b87fd1ee61ea2a7fc0619f805b0103
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-09-17 23:31:13 +0800

    Update 实验1.java

--- 实验1.java
+++ 实验1.java
@@ -1,1 +1,29 @@
+package shiyan1;
+import java.util.*;
+public class shiyan1 {
+	public static void main(String[] args)
+	{
+		
+		double max = 0,min = 0,heg;
+		int x;
+		Scanner in = new Scanner(System.in);
+		System.out.println("qing shu ru xue sheng ti zhong :");
+		heg = in.nextDouble();
+		max = min = heg;
+		for(x = 1;heg>=0;x++)
+		{
+			heg = in.nextDouble();
+			if(heg<0)
+				break;
+			if(heg <min)
+				min = heg;
+			if(heg>max)
+				max = heg;
+			
+
+		}
+		System.out.println("max = "+max+"  min = "+min+"  ren shu wei:"+x);
+	}
+
+}
 
