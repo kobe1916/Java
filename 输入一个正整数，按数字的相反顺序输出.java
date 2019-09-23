507e013ad3b0d19125e37078ad264804b3e74536
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-09-23 19:57:00 +0800

    Update 输入一个正整数，按数字的相反顺序输出.java

--- 输入一个正整数，按数字的相反顺序输出.java
+++ 输入一个正整数，按数字的相反顺序输出.java
@@ -15,3 +15,20 @@ num/=10;
 System.out.println(str);
 }
 }
+
+
+/*import java.util.*;
+class hello{
+    public static void main(String[] args){
+        int a,b,c,d,x;
+        Scanner in = new Scanner(System.in);
+        System.out.println("请输入：");
+        x = in.nextInt();
+        a = (x/1000);
+        b = (x/100)%10;
+        c = (x/10)%10;
+        d = x%10;
+        System.out.println(""+d+""+c+""+b+""+a);
+    }
+}
+*/
