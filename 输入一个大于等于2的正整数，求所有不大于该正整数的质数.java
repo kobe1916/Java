274a20260efe4ff7c58c032a48d7fefb433e2f0e
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-09-28 22:52:50 +0800

    Update 输入一个大于等于2的正整数，求所有不大于该正整数的质数.java

--- 输入一个大于等于2的正整数，求所有不大于该正整数的质数.java
+++ 输入一个大于等于2的正整数，求所有不大于该正整数的质数.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+public class Main {
 
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+        int x=in.nextInt();
+        for (int i = 2; i <= 30; i++) {
+            boolean flag = true;
+            for (int j = 2; j < i; j++) {
+            if ( i % j == 0) {
+            flag = false;
+            break;
+            }
+            }
+            if(flag)
+            {System.out.print(i+" ");}
+            }
+            }
+                
+            
+
+    
+    }
