78ab259a8c0bb7d35bb75c25e42fe46ba3f87c18
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-10-30 16:29:09 +0800

    Update 方法.java

--- 方法.java
+++ 方法.java
@@ -35,3 +35,34 @@ public class TestMethod {
     }
 
 }
+
+
+//重载的方法：实际是完全不同的方法，知识名称相同而已（形参个数、类型、顺序不同）
+
+
+public class TestOverload {
+    public static void main(String[] args){
+        System.out.println(add(3,5));
+        System.out.println(add(2,3,4));
+
+    }
+    public static int add(int n1,int n2){
+        int sum = n1+n2;
+        return sum;
+    }
+   
+    //调用非static方法时必须要在main中new一个新的对象才能调用，
+    // 加static后可以直接在main方法中调用
+    //个数不同构成重载
+    public static /*加static方便调用*/int add(int n1,int n2,int n3){
+        int sum = n1+n2+n3;
+        return sum;
+    }
+    
+    //类型不同构成重载
+    //顺序不同构成重载
+    
+}
+//只有参数名称不同不构成重载
+//只有返回值不同不构成重载
+//System.out.println()  就是一个方法的重载   打印类型、个数不同
