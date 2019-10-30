0fb092c687a19cffe0672524bfa7bde4d28158b6
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-10-30 15:23:29 +0800

    Update Scanner.java

--- Scanner.java
+++ Scanner.java
@@ -1,1 +1,17 @@
+import java.util.*;
+public class Scannner {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入你的名字：>");
+        String name = scanner.nextLine();
+        System.out.println("请输入你的爱好：>");
+        String favor = scanner.nextLine();
+        System.out.println("请输入你的年龄：>");
+        int age = scanner.nextInt();
 
+        System.out.println(name);
+        System.out.println(favor);
+        System.out.println(age);
+    }
+}
