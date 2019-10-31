47de733e59963b17ac27b5799b9ca792ae162f6e
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-10-31 17:58:17 +0800

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+/*
+定义Rectangle类，使其具有以下成员方法：
+class  Rectangle{
+    public Rectangle();               //将矩形的宽和高都设为1
+public Rectangle(int w, int h);
+public Rectangle(Rectangle r);
+public double getArea();          //计算矩形面积
+public double getPerimeter();     //计算矩形周长
+public int getWidth();            //返回矩形的宽
+public int getHeight();           //返回矩形的长
+public String toString(); //以格式“矩形（w，h）”返回当前矩形的字符串表示
+    public boolean equals(Rectangle  r);//测试矩形是否相等
+}
+定义测试类测试该类。
+
+*/
