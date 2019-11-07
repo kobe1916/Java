c421ad2e1414eab894f4bb0e901da3dd2615040d
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-11-07 23:34:51 +0800

    Create stu类.java

---
+++ stu类.java
@@ -0,0 +1,30 @@
+public class SxtStu{
+
+  //属性
+  int id;
+  String name;
+  int age;
+  
+  //方法
+  void study(){
+  System.out.println("");
+  }
+  
+  
+  void play(){
+  System.out.println();
+  }
+  
+  //程序的入口
+  public static void main(String[] args){
+    SxtStu stu = new SxtStu();
+    stu.play();
+    }
+    }
+  
+  
+  
+  
+  
+  
+  
