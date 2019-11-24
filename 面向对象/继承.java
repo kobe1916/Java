b2c5f14c4876ed2c9d4641c6840b37248dac7f3e
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-11-24 23:44:01 +0800

    Update 继承.java

--- 面向对象/继承.java
+++ 面向对象/继承.java
@@ -1,12 +1,13 @@
+/*
 1.父类也称超类、基类、派生类
 2.Java中只有单继承，没有像c++中的多继承，多继承会引起混乱，是的继承链过于复杂，系统难于维护
 3.Java中没有多继承，接口有多继承
 4.子类继承父类，可以得到父类的全部属性和方法（除了父类的构造方法），但不见得可以直接访问（比如，父类私有的属性和方法）
 5.定义一个 类时，没有调用extends，则它的父类时：java.lang.Object
-
+*/
 
 //ctrl+T可以方便的查看类的继承层次结构
-使用extends实现继承
+//使用extends实现继承
 public class TestExtend{
       public static void main(String[] args){
 	Student stu = new Student();
