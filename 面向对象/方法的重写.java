327227979da6a1770611e3df7fa2a63a550daec4
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-11-26 17:58:24 +0800

    Update 方法的重写.java

--- 面向对象/方法的重写.java
+++ 面向对象/方法的重写.java
@@ -8,6 +8,24 @@
 3. “>=" 访问权限，子类大于等于父类
 */
 
+
+/*             java方法重写的规定：
+
+方法的声明： 权限修饰符 返回值类型 方法名（形参列表） throws 异常的类型{  
+// 方法体
+}
+子类中的叫重写的方法， 父类中的叫被重写的方法
+子类重写的方法的方法名和形参列表与父类被重写的方法的方法名和形参列表相同
+子类重写的方法的权限修饰符不小于父类被重写的方法的权限修饰符
+特殊情况：子类不能重写父类被声明为private权限的方法
+返回值类型：
+父类是void，子类只能是void
+父类是A类型，子类是A类或A类的子类
+父类是基本数据类型（如：double），子类返回值类型必须相同
+
+子类重写的方法抛出的异常的类型不大于父类被重写的方法抛出的异常类型
+https://www.cnblogs.com/heliusKing/p/10835031.html
+*/
 package 面向对象.cn;
 
 /**
@@ -30,9 +48,11 @@ class Vehicle{
         System.out.println("stop!");
     }
     public Person whoIsPsg(){
+         /*  如果是一个类作为返回类型可以说是返回一个对象,
+        返回后可以调用它的getter,setter方法*/
         return new Person();
-
     }
+    
 }
 
 class Horse extends Vehicle{
