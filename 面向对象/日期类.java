8973c2b172714874a1dfe41b636de70b16318f56
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-12-18 21:29:04 +0800

    Create 日期类.java

---
+++ 面向对象/日期类.java
@@ -0,0 +1,19 @@
+mport java.text.SimpleDateFormat;
+import java.util.Calendar;
+public class rl{
+public static void main(String[] args) {
+SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+Calendar cal = Calendar.getInstance();
+
+cal.set(1949, 10, 1);
+long stime = cal.getTimeInMillis();
+cal.set(2010, 8, 15);
+long etime = cal.getTimeInMillis();
+
+long jtime = (etime - stime)/(1000*60*60*24);
+System.out.println(jtime);
+}
+
+
+}
