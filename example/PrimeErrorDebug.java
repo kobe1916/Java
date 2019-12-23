6f6c242ddac5135ce7858f9be00d74b6615c0ece
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-12-23 23:13:50 +0800

    Create PrimeErrorDebug.java

---
+++ example/PrimeErrorDebug.java
@@ -0,0 +1,11 @@
+public class PrimeErrorDebug {
+	public static void main(String[] args) {
+		System.out.println("main()方法开始运行了。");
+		//输出错误信息
+		System.out.println("在运行期间手动输出一个错误信息：");
+		System.out.println("\t 该软件没有买保险，请注意安全");
+		System.out.println("PrimeErrorAndDebug.main()");
+		System.out.println("main()方法运行结果。");
+		
+	}
+}
